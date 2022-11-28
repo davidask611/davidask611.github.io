b4faa203da666ca5ae25fe0d2f613be23f2d6644
david alvarez <davidask611@gmail.com>
2022-11-28 19:42:03 -0300

    "agregue javas"

---
+++ miscomandos.js
@@ -0,0 +1,17 @@
+function comprarMenu1() {
+  let nombrehambur = document.getElementByid("nombrehambur").innerhtml;
+  let valorhambur = parseInt(document.getElementById("valorhambur").innerHTML);
+  let cantmenu1 = parseInt(document.getElementById("cantidad").value);
+  console.log(valorhambur);
+  console.log(cantmenu1);
+  let confirmado = confirm(
+    "Vas a comprar " + nombrehambur + " que cuesta $" + valorhambur * cantmenu1
+  );
+  if (confirmado) {
+    let compraCofirmada = document.createElement("p");
+    compraCofirmada.innerHTML =
+      "Tu orden de " + nombrehambur + " costara $" + valorhambur * cantmenu1;
+    let pedidoagregado = document.getElementById("pedidonuevo");
+    pedidoagregado.appendChild(compraCofirmada);
+  }
+}
