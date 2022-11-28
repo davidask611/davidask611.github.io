07ef0b17b3795f37dc8f68bf6cc6a6c562185933
david alvarez <davidask611@gmail.com>
2022-11-28 19:58:47 -0300

    "modificaciones y javaScript"

--- miscomandos.js
+++ miscomandos.js
@@ -2,15 +2,15 @@ function comprarMenu1() {
   let nombrehambur = document.getElementByid("nombrehambur").innerhtml;
   let valorhambur = parseInt(document.getElementById("valorhambur").innerHTML);
   let cantmenu1 = parseInt(document.getElementById("cantidad").value);
-  console.log(valorhambur);
-  console.log(cantmenu1);
+  console.log(valorhambur)
+  console.log(cantmenu1)
   let confirmado = confirm(
-    "Vas a comprar " + nombrehambur + " que cuesta $" + valorhambur * cantmenu1
+    "Vas a comprar " + nombrehambur + " que cuesta $" + valorhambur*cantmenu1
   );
   if (confirmado) {
     let compraCofirmada = document.createElement("p");
     compraCofirmada.innerHTML =
-      "Tu orden de " + nombrehambur + " costara $" + valorhambur * cantmenu1;
+      "Tu orden de " + nombrehambur + " costara $" + valorhambur*cantmenu1;
     let pedidoagregado = document.getElementById("pedidonuevo");
     pedidoagregado.appendChild(compraCofirmada);
   }
